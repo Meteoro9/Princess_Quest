5014b96d4f8142043b933cf530410cd3c6525345
Pyrolex02 <matiasfreytes999@gmail.com>
2025-09-10 22:22:35 -0300

    Movimiento lateral y salto

---
+++ Princess-Quest/Assets/Code/Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+using Unity.Mathematics;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float _speed;
+    [SerializeField] float _jumpForce;
+    [SerializeField] float _groundCheckDistance;
+    [SerializeField] LayerMask _groundLayer;
+
+    Rigidbody rb;
+    float _moveH;
+    Vector3 _movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    void Update()
+    {
+        bool grounded = Physics.Raycast(transform.position, Vector3.down, _groundCheckDistance, _groundLayer);
+
+        _moveH = Input.GetAxis("Horizontal");
+
+        // Salto
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Movimiento horizontal
+        _movement = Vector3.right * _moveH * _speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + _movement);
+
+        // Rotación instantánea según la dirección
+        if (_moveH > 0.1f) // Derecha
+        {
+            rb.MoveRotation(Quaternion.Euler(0, 0f, 0));
+        }
+        else if (_moveH < -0.1f) // Izquierda
+        {
+            rb.MoveRotation(Quaternion.Euler(0, 180f, 0));
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        // Dibuja el raycast, VErde significa que toca el suelo, rojo que no 
+        bool grounded = Physics.Raycast(transform.position, Vector3.down, _groundCheckDistance, _groundLayer);
+        Gizmos.color = grounded ? Color.green : Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * _groundCheckDistance);
+        
+        // Dibuja una esfera pequeña al final del rayo
+        Gizmos.DrawSphere(transform.position + Vector3.down * _groundCheckDistance, 0.05f);
+    }
+}
