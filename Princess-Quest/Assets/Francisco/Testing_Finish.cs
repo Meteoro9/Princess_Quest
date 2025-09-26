29416d577771570a05760e09690c47b3681c471e
Francisco MT Mazzone <tolal.mazzone@gmail.com>
2025-09-26 19:41:37 -0300

    Ahora hay condición de victoria y muestra en pantalla plano verde al no haber enemigos

---
+++ Princess-Quest/Assets/Francisco/Testing_Finish.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Testing_Finish : MonoBehaviour
+{
+    private int enemiesInside = 0;
+
+    MeshRenderer greenPlane;
+
+    private void OnEnable()
+    {
+        greenPlane = GetComponentInChildren<MeshRenderer>();
+
+        greenPlane.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            enemiesInside++;
+            Debug.Log("Se añadió un enemigo a la cantidad" + enemiesInside);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            enemiesInside--;
+
+            if (enemiesInside < 0) enemiesInside = 0;
+
+            Debug.Log("Se quitó un enemigo de la cantidad");
+            //greenPlane.enabled = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (enemiesInside <= 0)
+        {
+            greenPlane.enabled = true;
+        }
+    }
+}
