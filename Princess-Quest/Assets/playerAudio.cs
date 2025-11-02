3a65f48f383cd0896361a76451d9f825dd108f65
joaquin-laorden <joaqunlaorden@gmail.com>
2025-11-02 16:07:17 -0300

    Commit final.

--- Princess-Quest/Assets/playerAudio.cs
+++ Princess-Quest/Assets/playerAudio.cs
@@ -2,19 +2,28 @@ using UnityEngine;
 
 public class playerAudio : MonoBehaviour
 {
-    
     AudioSource audioSource;
-    
-    [SerializeField] AudioClip pasos;
 
-    [SerializeField] AudioClip vozPrincesaAtaque;
+    [SerializeField]
+    AudioClip pasos;
 
-    [SerializeField] AudioClip saltoPrincesa;
+    [SerializeField]
+    AudioClip vozPrincesaAtaque;
+
+    [SerializeField]
+    AudioClip saltoPrincesa;
+
+    [SerializeField]
+    PlayerMovement playerMovement;
+
+    [SerializeField]
+    AttackComponent attackComponent;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        attackComponent.AttackStarted += PlayAttack;
     }
 
     // Update is called once per frame
@@ -22,21 +31,34 @@ public class playerAudio : MonoBehaviour
     {
         //float x = Input.GetAxis("Horizontal");
 
-        if (Input.GetButtonDown("Horizontal"))
+        if (Input.GetButtonDown("Horizontal") && playerMovement.IsGrounded)
         {
             audioSource.clip = pasos;
             audioSource.Play();
         }
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && playerMovement.IsGrounded)
         {
             audioSource.clip = saltoPrincesa;
             audioSource.Play();
         }
-        if (Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.X) || Input.GetKeyDown(KeyCode.J) || Input.GetKeyDown(KeyCode.K))
-        {
-            audioSource.clip = vozPrincesaAtaque;
-            audioSource.Play();
-        }
+        /*         if (IsAttackKeyPressed() && !attackComponent.IsAttacking)
+                {
+                    audioSource.clip = vozPrincesaAtaque;
+                    audioSource.Play();
+                } */
+    }
 
+    void PlayAttack(AttackSO attackSO)
+    {
+        audioSource.clip = vozPrincesaAtaque;
+        audioSource.Play();
+    }
+
+    bool IsAttackKeyPressed()
+    {
+        return Input.GetKeyDown(KeyCode.Z)
+            || Input.GetKeyDown(KeyCode.X)
+            || Input.GetKeyDown(KeyCode.J)
+            || Input.GetKeyDown(KeyCode.K);
     }
 }
